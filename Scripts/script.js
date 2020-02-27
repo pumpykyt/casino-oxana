abb95ae97a0437ad1e7c86a1b70ba08303dd9f46
AndrewHOME <senseandrey@gmail.com>
2020-02-28 00:25:20 +0200

    Main page + adaptive + footer fixed
    Footer fixed
    Added main-page
    Corrective responsive resolution.

--- Scripts/script.js
+++ Scripts/script.js
@@ -20,3 +20,27 @@ function checkTime(i) {
     }
     return i;
   }
+
+  $('.slide-nav').on('click', function(e) {
+    e.preventDefault();
+    // get current slide
+    var current = $('.flex-active').data('slide'),
+      // get button data-slide
+      next = $(this).data('slide');
+  
+    $('.slide-nav').removeClass('active');
+    $(this).addClass('active');
+  
+    if (current === next) {
+      return false;
+    } else {
+      $('.slider-wrapper').find('.flex-container[data-slide=' + next + ']').addClass('flex-preStart');
+      $('.flex-active').addClass('animate-end');
+      setTimeout(function() {
+        $('.flex-preStart').removeClass('animate-start flex-preStart').addClass('flex-active');
+        $('.animate-end').addClass('animate-start').removeClass('animate-end flex-active');
+      }, 800);
+    }
+  });
+
+  
