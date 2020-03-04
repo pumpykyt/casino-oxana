1470201b94583d64db0fb1ef0a533539cd3e76a0
Андрій Рижук <Ryzhuk@rivne.itstep.org>
2020-03-04 20:07:27 +0200

    user login displayed

---
+++ casino-oxana-back/casino-oxana-back/Controllers/LiveController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace casino_oxana_back.Controllers
+{
+    public class LiveController : Controller
+    {
+        // GET: Live
+        public ActionResult Live()
+        {
+
+            return View();
+        }
+    }
+}
