292e06b4f647d666dea4b18afe28bf77eeec7851
Viktor <37120451+pumpykyt@users.noreply.github.com>
2020-03-04 20:22:50 +0200

    skr
    table

---
+++ casino-oxana-back/casino-oxana-back/Entities/EFContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace casino_oxana_back.Entities
+{
+    public class EFContext
+    {
+    }
+}
---
+++ casino-oxana-back/casino-oxana-back/Entities/UserBalanceModel.cs
@@ -0,0 +1,26 @@
+﻿using casino_oxana_back.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace casino_oxana_back.Entities
+{
+    [Table("UserBalance")]
+    public class UserBalanceModel
+    {
+        [Key,ForeignKey("UserOf")]
+        public int Id { get; set; }
+        [Required]
+        public decimal Balance { get; set; }
+        [Required]
+        public decimal AllWinningsAmount { get; set; }
+        [Required]
+        public virtual ApplicationUser User { get; set; }
+        public string UserId { get; set; }
+
+
+    }
+}
