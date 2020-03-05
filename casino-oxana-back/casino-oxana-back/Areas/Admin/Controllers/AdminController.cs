24c3f0bdeb8f874c8c5bb66a2c7e67d24d1068ed
Андрій Рижук <Ryzhuk@rivne.itstep.org>
2020-03-05 20:11:34 +0200

    admin panel

---
+++ casino-oxana-back/casino-oxana-back/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace casino_oxana_back.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Admin
+        public ActionResult Admin()
+        {
+
+            return View();
+        }
+    }
+}
