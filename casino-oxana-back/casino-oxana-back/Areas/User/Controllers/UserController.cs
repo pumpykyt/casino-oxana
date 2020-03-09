5418ebc59c64ae25ef69d6cbf64896e122ffc092
Viktor <37120451+pumpykyt@users.noreply.github.com>
2020-03-09 18:40:59 +0200

    userarea
    usearea

--- casino-oxana-back/casino-oxana-back/Areas/User/Controllers/UserController.cs
+++ casino-oxana-back/casino-oxana-back/Areas/User/Controllers/UserController.cs
@@ -1,17 +1,36 @@
-﻿using System;
+﻿using casino_oxana_back.Models;
+using System;
 using System.Collections.Generic;
+using Microsoft.AspNet.Identity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using casino_oxana_back.Areas.User.Models;
+using System.Data.Entity;
 
 namespace casino_oxana_back.Areas.User.Controllers
 {
     public class UserController : Controller
     {
+        ApplicationDbContext _context = new ApplicationDbContext();
         // GET: User/User
-        public ActionResult Index()
+        public ActionResult Userloh()
         {
-            return View();
+            UserViewModel model = new UserViewModel();
+
+            
+            var userId = User.Identity.GetUserId();
+            var user = _context.Users.FirstOrDefault(t => t.Id == userId);
+            var userInfo = _context.userBalanceModels.FirstOrDefault(t => t.Id == userId);
+
+
+
+            model.Email = model.Email;
+            model.Balance = userInfo.Balance;
+
+
+            return View(user);
         }
     }
 }
